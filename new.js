c21d92f6fc86fa506ebee043612f27fc96800fbe
min <minwang.bus@gmail.com>
2023-02-13 10:55:43 -0500

    hackerankdate

---
+++ new.js
@@ -0,0 +1,9 @@
+let date = "02-13-2023"
+const newDate = new Date(date)
+const year = newDate.getFullYear()
+const month = newDate.getMonth() + 1
+const day = newDate.getDate()
+console.log(newDate)
+console.log(year)
+console.log(month)
+console.log(day)
